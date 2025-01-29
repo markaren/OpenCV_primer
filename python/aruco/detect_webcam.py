94a0963f4ff09962bf2c0764f9e08a4cc5b5af37
Lars Ivar Hatledal <laht@ntnu.no>
2025-01-29 10:09:49 +0100

    flip webcam frame

--- python/aruco/detect_webcam.py
+++ python/aruco/detect_webcam.py
@@ -11,6 +11,8 @@ if not cap.isOpened():
 while True:
     ret, image = cap.read()
 
+    image = cv2.flip(image, 1, image)
+
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # Define the dictionary of ArUco markers
--- python/webcam.py
+++ python/webcam.py
@@ -11,8 +11,9 @@ cv2.namedWindow("Webcam", cv2.WINDOW_AUTOSIZE)
 
 while True:
     ret, frame = cap.read()
-    cv2.imshow("Webcam", frame)
-    key = cv2.waitKey(1)
 
-    if key in [ord('q'), 27]: # q or ESC
+    frame = cv2.flip(frame, 1, frame) # flip
+    cv2.imshow("Webcam", frame)
+
+    if cv2.waitKey(1) in [ord('q'), 27]: # q or ESC
         break
